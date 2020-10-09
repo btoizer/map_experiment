d560960aae85eb316b17993ae5c757bfb9297df4
Barbara Toizer <67923295+btoizer@users.noreply.github.com>
2020-10-09 14:28:54 -0500

    Create map-task_main.js

---
+++ map-task_main.js
@@ -0,0 +1,9 @@
+ /* create timeline */
+  var timeline = [];
+
+  var trials = {
+    type: 'free-sort',
+    stimuli: ['img/africa_tag.png', 'img/asia_tag.png', 'img/europe_tag.png', 'img/n-america_tag.png', 'img/oceania_tag.png', 'img/s-america_tag.png']
+  };
+
+timeline.push(trials);
